513d95005ac2c8a8d9ebfbae9b583bfc2f9710f6
arun909 <arunbabu7591@gmail.com>
2024-07-31 18:01:32 +0530

    loop changes

--- loops.js
+++ loops.js
@@ -12,14 +12,11 @@ rl.question('ENTER ARRAY LIMIT: ', (input) => {
     var x = +input; // Convert input to a number
 
     // Ensure that x is a number and valid
-    if (!isNaN(x) && x >= 0) {
+    
         // Using a for loop to log values from x to 9
         for (let i = x; i < 10; i++) {
             console.log(i);
         }
-    } else {
-        console.log("Please enter a valid non-negative number.");
-    }
 
     // Close the readline interface
     rl.close();
