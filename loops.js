61e2aff2a1d2c3e3475603c56daac8ae955c49ef
arun909 <arunbabu7591@gmail.com>
2024-07-31 18:05:30 +0530

    loops change

--- loops.js
+++ loops.js
@@ -1,23 +1,22 @@
-const readline = require('readline');
+const readline = require('readline');   // Create an interface for input and output
 
-// Create an interface for input and output
-const rl = readline.createInterface
+const r2 = readline.createInterface
 ({
     input: process.stdin,
     output: process.stdout
 });
 
-// Prompt the user for the array limit
-rl.question('ENTER ARRAY LIMIT: ', (input) => {
+
+r2.question('ENTER ARRAY LIMIT: ', (input) => {  
     var x = +input; // Convert input to a number
 
     // Ensure that x is a number and valid
     
         // Using a for loop to log values from x to 9
-        for (let i = x; i < 10; i++) {
+        for (let i = x; i <= 50; i++) {
             console.log(i);
         }
 
     // Close the readline interface
-    rl.close();
+    r2.close();
 });
