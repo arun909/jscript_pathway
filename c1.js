674c74081e614c7adc7c2c4672f691503fa007ed
arun909 <arunbabu7591@gmail.com>
2024-08-17 19:45:58 +0530

    ce

---
+++ c1.js
@@ -0,0 +1,9 @@
+// there are two type of commenting is there in java script first one is this 
+
+/*
+  second one is this 
+*/
+
+//now the console out function
+
+console.log("%chello world", "color: red");
