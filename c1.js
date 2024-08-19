275cf296299417ea788d985c744d50c7740bf240
arun909 <arunbabu7591@gmail.com>
2024-08-19 19:37:59 +0530

    ce amd c1 deleted

--- c1.js
+++
@@ -1,9 +0,0 @@
-// there are two type of commenting is there in java script first one is this 
-
-/*
-  second one is this 
-*/
-
-//now the console out function
-
-console.log("%chello world", "color: red");
