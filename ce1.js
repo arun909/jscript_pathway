04629b56dd43201b79aea6d1ffb31f687917dff1
arun909 <arunbabu7591@gmail.com>
2024-08-21 22:56:42 +0530

    name change

--- ce1.js
+++
@@ -1,7 +0,0 @@
-// two kind of commenting this and 
-
-/*
-this 
-*/
-console.log("hey");
-console.log("we can use css files inside this");
---
+++ variables.js
@@ -0,0 +1,16 @@
+// two kind of commenting this and 
+
+/*
+this 
+*/
+
+//basic out put 
+console.log("hey");
+console.log("we can use css files inside this");
+
+
+//declaring variable and using it
+var name = "arun";
+console.log("hello", name);
+
+
